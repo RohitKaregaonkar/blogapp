0dcff5071e42ca1087721d459d0fb0b4d9f9e9b1
RohitKaregaonkar <karegaonkarrohit@gmail.com>
2023-10-14 11:43:14 +0530

    Adding tags to the post model and using ManyToManyField() to create many to many relationship

--- app/models.py
+++ app/models.py
@@ -2,14 +2,6 @@ from django.db import models
 from django.utils.text import slugify
 
 # Create your models here.
-class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    last_updated = models.DateTimeField(auto_now=True)
-    slug = models.SlugField(max_length=200, unique=True)
-    image = models.ImageField(null=True, blank=True, upload_to="images/")
-    
-
 class Tag(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=100)
@@ -22,3 +14,13 @@ class Tag(models.Model):
     
     def __str__(self):
         return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    last_updated = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    image = models.ImageField(null=True, blank=True, upload_to="images/")
+    tags =  models.ManyToManyField(Tag, blank=True, related_name='post')
+    
