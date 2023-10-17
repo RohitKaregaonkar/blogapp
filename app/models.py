0a56231c33f3b98aa87a4069bba7a1481e8391a0
RohitKaregaonkar <karegaonkarrohit@gmail.com>
2023-10-17 14:48:39 +0530

    Modified models.py file to add featured field in post model

--- app/models.py
+++ app/models.py
@@ -25,6 +25,7 @@ class Post(models.Model):
     image = models.ImageField(null=True, blank=True, upload_to="images/")
     tags =  models.ManyToManyField(Tag, blank=True, related_name='post')
     view_count = models.IntegerField(null=True, blank=True)
+    featured = models.BooleanField()
     
     
 class Comments(models.Model):
