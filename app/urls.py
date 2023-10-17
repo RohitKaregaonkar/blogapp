ef20404f4c587f4be3566870e35bc1c310731bc2
RohitKaregaonkar <karegaonkarrohit@gmail.com>
2023-10-17 15:25:15 +0530

    Modified urls.py file to add path to tags page

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,5 @@ from app import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('post/<slug:slug>', views.post_page, name='post_page'),
+    path('tag/<slug:slug>', views.tag_page, name='tag_page'),
 ]
