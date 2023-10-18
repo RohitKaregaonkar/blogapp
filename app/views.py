800e17591d4953717f54d9d5df33c5e7aa6a8461
RohitKaregaonkar <karegaonkarrohit@gmail.com>
2023-10-18 15:46:49 +0530

    Modified views.py file to create session instance for subscribe,  and index.html file to use session object to check whether user is subscribed or not

--- app/views.py
+++ app/views.py
@@ -23,6 +23,7 @@ def index(request):
         subscribe_form = SubscribeForm(request.POST)
         if subscribe_form.is_valid():
             subscribe_form.save()
+            request.session['subscribed'] = True
             subscribe_success = "Subscribed Successfully"  
             subscribe_form = SubscribeForm()
           
