67c220365b50450b842f768983c9a0611d58ad85
RohitKaregaonkar <karegaonkarrohit@gmail.com>
2023-10-18 16:52:48 +0530

    Added redirection url to home page in setting.py file

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -119,9 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
 MEDIA_ROOT = BASE_DIR / 'uploads'
 MEDIA_URL = '/media/'
+
+LOGIN_REDIRECT_URL = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
