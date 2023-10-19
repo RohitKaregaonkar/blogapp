629ab9acaf204df6e58e81d8d99a407fdade15e9
RohitKaregaonkar <karegaonkarrohit@gmail.com>
2023-10-19 10:10:06 +0530

    Added placeholders for fields for registration page

--- app/forms.py
+++ app/forms.py
@@ -30,3 +30,10 @@ class NewUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = {'username', 'email', 'password1', 'password2'}
+        
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.fields['username'].widget.attrs['placeholder'] = "Enter Username"
+            self.fields['email'].widget.attrs['placeholder'] = "Enter Email"
+            self.fields['password1'].widget.attrs['placeholder'] = "Enter Password"
+            self.fields['password2'].widget.attrs['placeholder'] = "Confirm Password"
