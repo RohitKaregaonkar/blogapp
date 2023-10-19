67bbeac32c303fb8074b22c2a4ea8601c7667396
RohitKaregaonkar <karegaonkarrohit@gmail.com>
2023-10-19 10:18:01 +0530

    Added Validations for fields in registration form

--- app/forms.py
+++ app/forms.py
@@ -37,3 +37,26 @@ class NewUserForm(UserCreationForm):
             self.fields['email'].widget.attrs['placeholder'] = "Enter Email"
             self.fields['password1'].widget.attrs['placeholder'] = "Enter Password"
             self.fields['password2'].widget.attrs['placeholder'] = "Confirm Password"
+            
+        def clean_username(self):
+            username = self.cleaned_data['username'].lower()
+            new = User.objects.filter(username= username)
+            if new.count():
+                raise forms.ValidationError("User Already Exists!")
+            return username
+        
+        def clean_email(self):
+            email = self.cleaned_data['email'].lower()
+            new = User.objects.filter(email= email)
+            if new.count():
+                raise forms.ValidationError("Email Already Exists!")
+            return email
+        
+        def clean_password2(self):
+            password1 = self.cleaned_data['password1']
+            password2 = self.cleaned_data['password2']
+            if password1 and password2 and password1 != password2:
+                raise forms.ValidationError("Passwords Do Not Matcj!")
+            return password2
+        
+        
