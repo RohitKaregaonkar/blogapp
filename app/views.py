5d1ed11d83cf3780f3a81d5f10453112560970b6
RohitKaregaonkar <karegaonkarrohit@gmail.com>
2023-10-19 10:24:08 +0530

    Added csrf_token and post method and Updated register_user view to save the registration data and redirect to logged in page by verifying login

--- app/views.py
+++ app/views.py
@@ -3,6 +3,8 @@ from app.models import Post, Comments, Tag, Profile, WebsiteMeta
 from app.forms import CommentForm, SubscribeForm, NewUserForm
 from django.http import HttpResponse
 from django.urls import reverse
+from django.contrib.auth import login
+
 
 # Create your views here.
 def index(request):
@@ -106,5 +108,12 @@ def about_page(request):
 
 def register_user(request):
     form = NewUserForm()
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("/")
+        
     context = {'form': form}
     return render(request, 'registration/registration.html', context)
